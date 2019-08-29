04826e95ff7bde8f1b7a3c2db3ead65ea77523d8
Calvin Tjiang <calvin.tjiang@gmail.com>
2019-08-29 15:51:32 +1000

    Initial Commit

---
+++ LinearRegression.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self):
+    
+    def predict(self):
+        pass
+
+    def cost(self):
+        pass
+
+    def gradient_descent(self)
+        pass
+    
+    def normal_equation(self,):
+        pass
