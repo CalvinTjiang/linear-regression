fddb4f597e3284b194d2603d5fbe80a08e03ae82
Calvin Tjiang <calvin.tjiang@gmail.com>
2019-09-01 16:00:35 +1000

    Show the test result for LinearRegression Check cost, predict, and gradient descent method

--- test.py
+++ test.py
@@ -27,7 +27,7 @@ linear_regression = lr.LinearRegression(2)
 x, y = generate_data(2, [-0.5, 0.2], [0.3, 1], (0, 10, 0.1))
 
 linear_regression.batch_size = 25
-linear_regression.total_epochs = 4
+linear_regression.total_epochs = 80
 print(linear_regression.cost(x, y))
 linear_regression.gradient_descent(x, y)
 print(linear_regression.cost(x, y))
@@ -37,14 +37,35 @@ linear_regression.gradient_descent(x, y)
 print(linear_regression.cost(x, y))
 linear_regression.gradient_descent(x, y)
 print(linear_regression.cost(x, y))
-'''
-# can only visualize the first 2 feature from
-# Initialize the 3D scatterplot
+
+# X = x[:,0]
+# Y = x[:,1]
+# print(X)
+# print(Y)
+# X, Y = np.meshgrid(X, Y)
+# print(X.shape)
+# print(Y.shape)
+
+# X = np.arange(-5, 5, 0.25)
+# Y = np.arange(-3, 3, 0.25)
+# print(X.shape)
+# print(Y.shape)
+# X, Y = np.meshgrid(X, Y)
+# print(X.shape)
+# print(Y.shape)
+# R = np.sqrt(X**2 + Y**2)
+# print(R.shape)
+# Z = np.sin(R)
+# print(R.shape)
+# # can only visualize the first 2 feature from
+# # Initialize the 3D scatterplot
+
+prediction = linear_regression.predict(x)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 # Draw the point
 ax.scatter(x[:,0], x[:,1], y)
+ax.scatter(x[:,0], x[:,1], prediction, marker='^')
 
 # Add the label
 ax.set_xlabel('Feature 1')
@@ -53,4 +74,4 @@ ax.set_zlabel('Output')
 
 # Show the 3D scatterplot
 plt.show()
-'''
+
